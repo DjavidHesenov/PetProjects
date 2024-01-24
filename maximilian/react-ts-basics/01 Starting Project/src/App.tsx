48aad45bb158719a976c95f24e62c969dc23ec00
DjavidHesenov <hesenovdjavid@gmail.com>
2024-01-24 20:45:34 +0400

    first commit

---
+++ maximilian/react-ts-basics/01 Starting Project/src/App.tsx
@@ -0,0 +1,11 @@
+import CourseGoal from "./components/CourseGoal.tsx";
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal title="Learn React + TS">
+        From the ground!
+      </CourseGoal>
+    </main>
+  );
+}
