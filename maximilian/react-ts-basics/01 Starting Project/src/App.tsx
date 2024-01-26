addfbc83704792127138052cf87d360c13b3adf7
DjavidHesenov <hesenovdjavid@gmail.com>
2024-01-26 18:30:15 +0400

    S3 done

--- maximilian/react-ts-basics/01 Starting Project/src/App.tsx
+++ maximilian/react-ts-basics/01 Starting Project/src/App.tsx
@@ -1,11 +1,37 @@
-import CourseGoal from "./components/CourseGoal.tsx";
+import CourseGoalList from './components/CourseGoalList.tsx';
+import goalsImg from './assets/goals.jpg'
+import Header from "./components/Header.tsx";
+import { useState } from "react";
+import NewGoal from './components/NewGoal.tsx';
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number
+}
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([])
+
+  const handleAddGoal = (goal: string, summary: string) => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = { id: Math.random(), description: summary, title: goal }
+      return [...prevGoals, newGoal]
+    })
+  }
+
+  const handleDeleteGoal = (id: number) => {
+    setGoals(prevGoals => prevGoals.filter((goal) => goal.id !== id))
+  }
+
   return (
     <main>
-      <CourseGoal title="Learn React + TS">
-        From the ground!
-      </CourseGoal>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }} >
+        <h1>Your Course Goals</h1>
+      </Header>
+      {/* <button onClick={handleAddGoal} >Add Goal</button> */}
+      <NewGoal onAddGoal={handleAddGoal}/>
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
 }
--- maximilian/react-ts-basics/01 Starting Project/src/components/CourseGoal.tsx
+++ maximilian/react-ts-basics/01 Starting Project/src/components/CourseGoal.tsx
@@ -5,15 +5,18 @@ import { type PropsWithChildren, type ReactNode } from "react"
 //     children: ReactNode
 // }
 
-type CourseGoalProps = PropsWithChildren<{ title: string }>
+type CourseGoalProps = PropsWithChildren<{
+    title: string, onDelete: (id: number) => void, id: number
+}>
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+export default function CourseGoal({ title, children, onDelete, id }: CourseGoalProps) {
 
     return <article>
         <div>
             <h2>{title}</h2>
             {children}
         </div>
-        <button>Delete</button>
+        <button onClick={(event) => onDelete(id)} >Delete</button>
     </article>
 }
+
